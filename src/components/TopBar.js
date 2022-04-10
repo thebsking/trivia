1d67d3fcb3ae8c6d6c7a211fd0cab1f60c8f6c21
Brandon King <brandonscottking@gmail.com>
2022-04-10 19:07:45 -0400

    changed homepage to topbar component

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,13 +1,13 @@
 import { Button, Container } from '@mui/material';
 import React from 'react';
 
-function Home() {
+function TopBar() {
   return(
     <div className='content'>
       <div>
         <h1>Alan's Trivia Database</h1>
       </div>
-      <Container >
+      <Container id="menu-buttons">
         <Button variant='contained'>Search Database</Button>
         <Button variant='contained'>Add Question to DB</Button>
         <Button variant='contained' disabled>Generate Game</Button>
@@ -17,4 +17,4 @@ function Home() {
   )
 }
 
-export default Home;
+export default TopBar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './pages/Home'
+import TopBar from './components/TopBar'
 import AddQuestion from './pages/AddQuestion';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <TopBar />
   </React.StrictMode>,
   document.getElementById('root')
 );
